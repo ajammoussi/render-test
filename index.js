1b9fd7ebbf9189ec4d57a5622b7400d6680e0044
ajammoussi <jam.ahmed55@gmail.com>
2024-07-21 17:45:17 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const Note = require("./models/note");
-const app = express();
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const Note = require("./models/note")
+const app = express()
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static("dist"));
+app.use(express.json())
+app.use(cors())
+app.use(express.static("dist"))
 
 
 let notes = [
@@ -25,79 +25,102 @@ let notes = [
         content: "GET and POST are the most important methods of HTTP protocol",
         important: true,
     },
-    ];
+]
 
-    const requestLogger = (request, response, next) => {
-    console.log("Method:", request.method);
-    console.log("Path:  ", request.path);
-    console.log("Body:  ", request.body);
-    console.log("---");
+const requestLogger = (request, response, next) => {
+    console.log("Method:", request.method)
+    console.log("Path:  ", request.path)
+    console.log("Body:  ", request.body)
+    console.log("---")
     next();
-    };
+};
 
-    app.use(requestLogger);
+app.use(requestLogger)
 
-    const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: "unknown endpoint" });
-    };
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" })
+    }
 
-    app.get("/", (request, response) => {
-    response.send("<h1>Hello World!</h1>");
-    });
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World!</h1>")
+    })
 
-    app.get("/api/notes", (request, response) => {
+app.get("/api/notes", (request, response) => {
     Note.find({}).then((notes) => {
         response.json(notes);
-    });
-    });
+    })
+    })
 
-    app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    const note = notes.find((note) => note.id === id);
+app.get("/api/notes/:id", (request, response) => {
+    // without MongoDB
+    // const id = request.params.id
+    // const note = notes.find((note) => note.id === id)
 
-    if (note) {
+    // if (note) {
+    //     response.json(note)
+    // } else {
+    //     response.status(404).end()
+    // }
+
+    // with MongoDB
+    Note.findById(request.params.id).then((note) => {
         response.json(note);
-    } else {
-        response.status(404).end();
-    }
-});
+    });
+})
 
 app.delete("/api/notes/:id", (request, response) => {
-    const id = request.params.id;
-    notes = notes.filter((note) => note.id !== id);
+    // without MongoDB
+    // const id = request.params.id;
+    // notes = notes.filter((note) => note.id !== id)
+    // response.status(204).end()
 
-    response.status(204).end();
-});
+    // with MongoDB
+    Note.findOneAndDelete({ _id: request.params.id })
+        .then((result) => {
+            response.status(204).end()
+        })
+        .catch((error) => next(error))
+})
 
 const generateId = () => {
     const maxId =
         notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-    return String(maxId + 1);
+    return String(maxId + 1)
 };
 
 app.post("/api/notes", (request, response) => {
-    const body = request.body;
+    const body = request.body
 
     if (!body.content) {
         return response.status(400).json({
         error: "content missing",
-        });
+        })
     }
 
-    const note = {
+    // without MongoDB
+    // const note = {
+    //     content: body.content,
+    //     important: body.important || false,
+    //     id: generateId()
+    // };
+    // notes = notes.concat(note)
+    // response.json(note)
+
+
+    // with MongoDB
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    };
+    })
 
-    notes = notes.concat(note);
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
+})
 
-    response.json(note);
-});
-
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
