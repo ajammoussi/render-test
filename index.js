039086d621876ff1cfd1c11ccd953bb225c5ddb2
ajammoussi <jam.ahmed55@gmail.com>
2024-07-21 20:06:54 +0100

    uibuild

--- index.js
+++ index.js
@@ -131,11 +131,6 @@ app.post("/api/notes", (request, response, next) => {
 app.put("/api/notes/:id", (request, response, next) => {
     const { content, important } = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
     Note.findByIdAndUpdate(
         request.params.id,
         { content, important },
